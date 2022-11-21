d50766884c222b106020be817a2649b928a38311
Kovács Dorina <kovacsdorina2@gmail.com>
2022-11-21 10:15:25 +0100

    Függvény javítás kész

--- src/views/MainWindow.java
+++ src/views/MainWindow.java
@@ -59,90 +59,139 @@ public class MainWindow extends JFrame {
     public StatusBar statusBar;
 
     public MainWindow() {
-        appJLabel = new JLabel("Szógyűjtő");
-        urlJTextField = new JTextField();        
+            appJLabel = new JLabel("Szógyűjtő");
+            urlJTextField = new JTextField();        
+   
+            collectorJPanel = new JPanel();
+            collectorJPanel.setLayout(new BoxLayout(collectorJPanel, BoxLayout.LINE_AXIS));
+            pasteJButton = new JButton("Beillesztés");
+            startJButton = new JButton("Indít");
+            aboutJButton = new JButton("Névjegy");
+            exitJButton = new JButton("Kilépés");
 
-        collectorJPanel = new JPanel();
-        collectorJPanel.setLayout(new BoxLayout(collectorJPanel, BoxLayout.LINE_AXIS));
-        pasteJButton = new JButton("Beillesztés");
-        startJButton = new JButton("Indít");
-        aboutJButton = new JButton("Névjegy");
-        exitJButton = new JButton("Kilépés");
-
-        this.collectorJPanel.add(pasteJButton);
-        this.collectorJPanel.add(startJButton);
-        this.collectorJPanel.add(aboutJButton);
-        this.collectorJPanel.add(exitJButton);
-
-
-        listJPanel = new JPanel();
-        listJPanel.setLayout(new BoxLayout(listJPanel, BoxLayout.PAGE_AXIS));
-        FoundWordsJLabel = new JLabel("Talált szavak");
-
-        wordsDefaultListModel = new DefaultListModel<>();
-        wordsJList = new JList<>(wordsDefaultListModel);
-        wordsJScrollPane = new JScrollPane(wordsJList);
-        listJPanel.add(FoundWordsJLabel);
-        listJPanel.add(wordsJScrollPane);
-
-        deleteJButton = new JButton("Törlés");
-        deleteJButton.setMinimumSize(new Dimension(110, 25));
-        deleteJButton.setMaximumSize(new Dimension(110, 25));
-        deleteJButton.setPreferredSize(new Dimension(110, 25));
-        deleteJButton.setMargin(new Insets(20, 20, 20, 20));
-
-        sortJButton = new JButton("Rendezés");
-        sortJButton.setMinimumSize(new Dimension(110, 25));
-        sortJButton.setMaximumSize(new Dimension(110, 25));
-        sortJButton.setPreferredSize(new Dimension(110, 25));
-        
-        filterJButton = new JButton("Szűrés");
-        filterJButton.setMinimumSize(new Dimension(110, 25));
-        filterJButton.setMaximumSize(new Dimension(110, 25));
-        filterJButton.setPreferredSize(new Dimension(110, 25));
-
-        saveJButton = new JButton("Mentés");
-        saveJButton.setMinimumSize(new Dimension(110, 25));
-        saveJButton.setMaximumSize(new Dimension(110, 25));
-        saveJButton.setPreferredSize(new Dimension(110, 25));
-        
-
-
-        listButtonsJPanel = new JPanel();
-        listButtonsJPanel.setLayout(new BoxLayout(listButtonsJPanel, BoxLayout.PAGE_AXIS));
-        listButtonsJPanel.add(Box.createVerticalGlue());
-        listButtonsJPanel.add(Box.createVerticalGlue());
-        listButtonsJPanel.add(deleteJButton);
-        listButtonsJPanel.add(Box.createVerticalGlue());
-        listButtonsJPanel.add(sortJButton);
-        listButtonsJPanel.add(Box.createVerticalGlue());
-        listButtonsJPanel.add(filterJButton);
-        listButtonsJPanel.add(Box.createVerticalGlue());
-        listButtonsJPanel.add(saveJButton);
-        listButtonsJPanel.add(Box.createVerticalGlue());
-        listButtonsJPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-        bottomJPanel = new JPanel();
-        bottomJPanel.setLayout(new BoxLayout(bottomJPanel, BoxLayout.LINE_AXIS));
-        bottomJPanel.add(listJPanel);
-        bottomJPanel.add(listButtonsJPanel);
-        
-
-
-        this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
-
-        this.add(appJLabel);
-        this.add(urlJTextField);
-        this.add(collectorJPanel);
-        this.add(bottomJPanel);
-        
-        
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-        this.pack();
+        CollectorPanel();
+        }
        
-        statusBar = new StatusBar(this.getWidth());
-        this.add(statusBar);
+
+        public void CollectorPanel(){
+
+            this.collectorJPanel.add(pasteJButton);
+            this.collectorJPanel.add(startJButton);
+            this.collectorJPanel.add(aboutJButton);
+            this.collectorJPanel.add(exitJButton);
+
+        ListWords();
+        }
+      
+        public void ListWords(){
+        
+            listJPanel = new JPanel();
+            listJPanel.setLayout(new BoxLayout(listJPanel, BoxLayout.PAGE_AXIS));
+            FoundWordsJLabel = new JLabel("Talált szavak");
+
+        DefaultListModel();
+        }
+
+        public void DefaultListModel(){
+
+            wordsDefaultListModel = new DefaultListModel<>();
+            wordsJList = new JList<>(wordsDefaultListModel);
+            wordsJScrollPane = new JScrollPane(wordsJList);
+            listJPanel.add(FoundWordsJLabel);
+            listJPanel.add(wordsJScrollPane);
+
+        Delete();
+        }
+
+        public void Delete(){
+
+            deleteJButton = new JButton("Törlés");
+            deleteJButton.setMinimumSize(new Dimension(110, 25));
+            deleteJButton.setMaximumSize(new Dimension(110, 25));
+            deleteJButton.setPreferredSize(new Dimension(110, 25));
+            deleteJButton.setMargin(new Insets(20, 20, 20, 20));
+
+        Sort();
+        }
+     
+        public void Sort(){
+
+            sortJButton = new JButton("Rendezés");
+            sortJButton.setMinimumSize(new Dimension(110, 25));
+            sortJButton.setMaximumSize(new Dimension(110, 25));
+            sortJButton.setPreferredSize(new Dimension(110, 25));
+
+        Filter();
+        }
+
+        public void Filter(){
+
+            filterJButton = new JButton("Szűrés");
+            filterJButton.setMinimumSize(new Dimension(110, 25));
+            filterJButton.setMaximumSize(new Dimension(110, 25));
+            filterJButton.setPreferredSize(new Dimension(110, 25));
+
+        Save();
+        }
+
+        public void Save(){
+
+            saveJButton = new JButton("Mentés");
+            saveJButton.setMinimumSize(new Dimension(110, 25));
+            saveJButton.setMaximumSize(new Dimension(110, 25));
+            saveJButton.setPreferredSize(new Dimension(110, 25));
+
+        ListButtons();
+        }
+        
+        public void ListButtons(){
+            
+            listButtonsJPanel = new JPanel();
+            listButtonsJPanel.setLayout(new BoxLayout(listButtonsJPanel, BoxLayout.PAGE_AXIS));
+            listButtonsJPanel.add(Box.createVerticalGlue());
+            listButtonsJPanel.add(Box.createVerticalGlue());
+            listButtonsJPanel.add(deleteJButton);
+            listButtonsJPanel.add(Box.createVerticalGlue());
+            listButtonsJPanel.add(sortJButton);
+            listButtonsJPanel.add(Box.createVerticalGlue());
+            listButtonsJPanel.add(filterJButton);
+            listButtonsJPanel.add(Box.createVerticalGlue());
+            listButtonsJPanel.add(saveJButton);
+            listButtonsJPanel.add(Box.createVerticalGlue());
+            listButtonsJPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            
+        BottomPanel();
+        }
+
+        public void BottomPanel(){
+
+            bottomJPanel = new JPanel();
+            bottomJPanel.setLayout(new BoxLayout(bottomJPanel, BoxLayout.LINE_AXIS));
+            bottomJPanel.add(listJPanel);
+            bottomJPanel.add(listButtonsJPanel);
+
+        Add();
+        }
+
+        public void Add(){
+
+            this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+
+            this.add(appJLabel);
+            this.add(urlJTextField);
+            this.add(collectorJPanel);
+            this.add(bottomJPanel);
+            
+        Status();
+        }
+       
+        public void Status(){
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+            this.pack();
+        
+            statusBar = new StatusBar(this.getWidth());
+            this.add(statusBar);
 
     }
     
